557602adef3c02c4833eb5a694a48b00cfbeab9d
Lorennaz <lorennazaqui@estudante.ufscar.br>
2026-06-30 13:08:37 -0300

    Create LED.cpp

---
+++ LED.cpp
@@ -0,0 +1,21 @@
+#include "LED.h"
+
+#include <fstream>
+
+LED::LED(const std::string& arquivo) {
+    nomeArquivo = arquivo;
+}
+
+void LED::adicionar(int rrn) {
+
+}
+
+bool LED::obter(int &rrn) {
+
+    return false;
+}
+
+bool LED::vazia() {
+
+    return true;
+}
