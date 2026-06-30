8645ad661e90f6527f02a82d4bfb5ea3ca95cc6e
Lorennaz <lorennazaqui@estudante.ufscar.br>
2026-06-30 13:11:38 -0300

    Update LED.cpp

--- LED.cpp
+++ LED.cpp
@@ -2,20 +2,52 @@
 
 #include <fstream>
 
-LED::LED(const std::string& arquivo) {
+using namespace std;
+
+LED::LED(const string& arquivo) {
     nomeArquivo = arquivo;
 }
 
 void LED::adicionar(int rrn) {
+    ofstream arquivo(nomeArquivo, ios::binary | ios::app);
 
+    if (!arquivo.is_open())
+        return;
+
+    arquivo.write(reinterpret_cast<char*>(&rrn), sizeof(int));
+
+    arquivo.close();
 }
 
 bool LED::obter(int &rrn) {
+    ifstream arquivo(nomeArquivo, ios::binary);
+
+    if (!arquivo.is_open())
+        return false;
+
+    arquivo.seekg(0, ios::end);
+
+    if (arquivo.tellg() == 0) {
+        arquivo.close();
+        return false;
+    }
+
+    arquivo.close();
 
     return false;
 }
 
 bool LED::vazia() {
+    ifstream arquivo(nomeArquivo, ios::binary);
 
-    return true;
+    if (!arquivo.is_open())
+        return true;
+
+    arquivo.seekg(0, ios::end);
+
+    bool resultado = (arquivo.tellg() == 0);
+
+    arquivo.close();
+
+    return resultado;
 }
